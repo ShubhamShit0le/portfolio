782dcdde94096a8ced4178d192b78e669c2028db
ShubhamShit0le <shubhamshitole099@gmail.com>
2023-05-30 23:24:21 +0530

    Project.jsx add

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
+import React from 'react';
 
+const Project = () => {
+  return (
+    <div className="project-page">
+      <div className="project-title">
+        {/* Project title content */}
+      </div>
+      <div className="project-description">
+        {/* Project description content */}
+      </div>
+      <div className="project-technology">
+        {/* Project technology content */}
+      </div>
+      <div className="project-snippets">
+        {/* Project snippet images */}
+      </div>
+    </div>
+  );
+};
 
-const Project = (props) => {
-    const id = props.id;
-    const fullProject = () => {
-   /*
-   project = select * from project where id = {id};
-   */ 
-  }
-
-    // const {
-    //       title,description,technology
-          
-    // } = props.project;
-    // return (
-
-    //     <div> 
-    //         {title}
-    //         {description}
-    //         {technology}
-
-    //     </div>
-    // )
-}
-
+export default Project;
