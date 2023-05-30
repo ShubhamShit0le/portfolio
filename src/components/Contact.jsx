32b9273128b86ee5db797f4ecb4afb03a866e14f
ShubhamShit0le <shubhamshitole099@gmail.com>
2023-05-30 12:43:51 +0530

    adding api in Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 const Contact = () => {
   return (
+    <form
+    action="https://formspree.io/f/xnqyyyao"
+    method="POST"
+    >
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <div className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8 flex flex-col justify-center w-full h-full items-center'>
@@ -12,8 +16,12 @@ const Contact = () => {
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
             <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
             <button className='text-white border-2 hover:bg-cyan-500 hover:border-cyan-500 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+        
+           
         </div>
+        
     </div>
+    </form>
   )
 }
 export default Contact
